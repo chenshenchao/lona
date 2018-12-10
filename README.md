6fdb239a12cb7a04fa8363248c5eb3d190d7f288
chenshenchao <chenshenchao@outlook.com>
2018-12-11 00:48:48 +0800

    good luck.

---
+++ README.md
@@ -0,0 +1,1 @@
+# [Lona](https://github.com/chenshenchao/lona)
---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(application)
+    application.identity = "Lona"
+    application.console = false
+
+    application.window.title = "Lona"
+end
